9b1fae8ab17ff330d97893abf9caf549c71c7a34
Punit Tewani <tewanipunit1233@gmail.com>
2022-05-25 12:03:39 +0530

    Mocha Testing

---
+++ spec/User-service-test.spec.ts
@@ -0,0 +1,12 @@
+import App from '../app';
+import chai , {expect} from 'chai';
+import chaiHttp from 'chai-http';
+const app = new App()
+describe('User-Service', () => { 
+    it('Checking user register')
+
+  
+
+
+
+});
